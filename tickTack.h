f39bfa7939ffa04d1771a13a0ae6581f5e37be9e
ArturHamerski98 <100742290+ArturHamerski98@users.noreply.github.com>
2022-06-01 21:53:55 +0200

    alpha2.0

--- tickTack.h
+++ tickTack.h
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+#include <time.h>
+
 void menu();
 void modeChoose();
 void playAgain();
@@ -8,8 +13,13 @@ void twoPlayerName();
 void printBoard(char board[3][3]);
 int  isNumber(char number[]);
 char checkWinner(char board[3][3]);
-char checkHypoteticWinner(char board[3][3],int x, int y,char player_sign);
+char checkHypoteticWinner(char board[3][3], int x, int y, char player_sign);
 void selectPoint(char board[3][3], char player_sign);
 int checkIfOccupied(int x, int y, char board[3][3]);
 int checkIfOutside(int x, int y);
 void computerMove(char board[3][3], int comp_level, char player_sign);
+void pVp();
+void cVc();
+void pVc();
+void offTimmer(clock_t start);
+void getPlayerName(char nick[30]);
