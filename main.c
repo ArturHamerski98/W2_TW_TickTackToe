72c9404c49fda616676dfde8ab72dd2005f74024
ArturHamerski98 <100742290+ArturHamerski98@users.noreply.github.com>
2022-06-02 05:01:36 +0200

    beta1.0

--- main.c
+++ main.c
@@ -1,49 +1,47 @@
 #include <stdio.h>
 #include <conio.h>
 #include <Windows.h>
-#include "tictac.h"
+#include "ticTac.h"
 #include <time.h>
 
-int main()
-{
-	while (1) {
-		menu();
+int main() {
+    while (1) {
+        menu();
 
-		char choice;
-		choice = _getch();
-		system("cls");
-		switch (choice)
-		{
-		case '1':
-		{
-			modeChoose();
-			choice = _getch();
-			system("cls");
-		
-			// PLAYER VS PLAYER
-			if (choice == '1')
-			{
-				pVp();
-			}
-			//COMPUTER VS COMPUTER
-			else if (choice == '2')
-			{
-				cVc();
-			}
-			//PLAYER VS COMPUTER
-			else if (choice == '3')
-			{
-				pVc();
-			}
-		}
-		break;
-		case '2':
-		{
-			return 0;
-		}
-		}
-	}
+        char choice;
+        choice = _getch();
+        system("cls");
+        switch (choice) {
+        case '1': {
+            modeChoose();
+            choice = _getch();
+            system("cls");
 
+            // PLAYER VS PLAYER
+            if (choice == '1') {
+                pVp();
+            }
+            //COMPUTER VS COMPUTER
+            else if (choice == '2') {
+                cVc();
+            }
+            //PLAYER VS COMPUTER
+            else if (choice == '3') {
+                pVc();
+            }
+            else if (choice == '4') {
+                printHallOfFame();
+            }
+            else if (choice == '5') {
+                statsOfAi();
+            }
+        }
+                break;
+        case '2': {
+            return 0;
+        }
+        }
+    }
 
-	return 0;
+    return 0;
 }
